38f4ba6268e7c86e279341487d9d45d3df219db5
Blake Bishop <blake.bishop@kuali.co>
2019-11-01 16:34:11 -0700

    Cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 import React from "react"
+import { Box } from "@chakra-ui/core"
 
 import Layout from "../components/layout"
+import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Welcome to the Picante Monster F2F</h1>
+    <Box d="flex" justifyContent="center">
+      <Box m="30px" w="50%">
+        <Image />
+      </Box>
+    </Box>
   </Layout>
 )
 
