d6d5803255d7775eba1bd48f523d452ba2a815cd
Drew Bowman <bowmande@gmail.com>
2019-11-02 17:03:48 -0600

    try react loadable

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react"
 import { Link } from "gatsby"
+import Loadable from 'react-loadable'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Calendar from "../components/calendar"
 import CurrentEvent from "../components/current-event"
 
 import { Text } from "@chakra-ui/core"
+
+const LoadableCalendar = Loadable({
+  loader: () => import('../components/calendar'),
+  loading() {
+    return <div>Loading...</div>
+  },
+})
 
 const CalendarPage = () => {
   const [info, setInfo] = useState(0);
@@ -17,7 +24,7 @@ const CalendarPage = () => {
       <Text fontSize="4xl">Schedule</Text>
       <Text fontSize="2xl">During the days on Tuesday and Wednesday, we will have water available, if you would like other snacks feel free to bring them and any drink you would like.</Text>
       <Text fontSize="1xl">Click an event for more details</Text>
-      <Calendar handleEventClick={(info) => setInfo(info)} />
+      <LoadableCalendar handleEventClick={(info) => setInfo(info)} />
       <CurrentEvent info={info} />
     </Layout>
   )
